eb0e018cfcc4b74a900a37fc97efc14194744936
sanjaipandi66@gmail.com <sanjaipandi66@gmail.com>
2025-04-02 13:09:03 +0530

    done

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 const express = require('express');
 const { resolve } = require('path');
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 const port = 3010;
 
+app.use(express.json());
 app.use(express.static('static'));
+
+mongoose.connect(process.env.MONGO_URI )
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+const User = mongoose.model('User', UserSchema);
+
+app.post("/register", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const hashpassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({ username, email, password: hashpassword });
+    await newUser.save();
+
+    res.status(201).json({ message: "User registration successful" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
